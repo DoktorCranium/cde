73cab7551102656671f6a9a8493b67c6399ac494
Liang Chang <l-chang@users.sourceforge.net>
2021-02-17 01:59:40 +0800

    dtappbuilder: fix issues of property settings for text pane.

--- cde/programs/dtappbuilder/src/ab/pal_textp.c
+++ cde/programs/dtappbuilder/src/ab/pal_textp.c
@@ -198,7 +198,8 @@ textp_prop_init(
     DtbRevolvPropDialogInfo	rpd = &(dtb_revolv_prop_dialog);
     PropTextpSettingsRec    	*pts = &(prop_textp_settings_rec[type]);
     Widget			item[10];
-    int				item_val[10];
+    XtArgVal		item_val[10];
+    XtPointer 		item_ptr[10];
     int				n;
 
     if (type == AB_PROP_REVOLVING)
@@ -268,38 +269,38 @@ textp_prop_init(
 	/* Scrolling */
         n = 0;
         item[n] = cgen->scroll_rbox_items.Never_item;
-        item_val[n] = AB_SCROLLBAR_NEVER; n++;
+        item_ptr[n] = AB_SCROLLBAR_NEVER; n++;
         item[n] = cgen->scroll_rbox_items.Always_item;
-        item_val[n] = AB_SCROLLBAR_ALWAYS; n++;
+        item_ptr[n] = AB_SCROLLBAR_ALWAYS; n++;
         prop_radiobox_init(&(pts->scrolling), cgen->scroll_rbox_label,
-                cgen->scroll_rbox, n, item, (XtPointer*)item_val,
+                cgen->scroll_rbox, n, item, item_ptr,
                 cgen->scroll_cb);
 
         /* Border Frame */
         n = 0;
         item[n] = cgen->bframe_opmenu_items.None_item;
-        item_val[n] = AB_LINE_NONE; n++;
+        item_ptr[n] = AB_LINE_NONE; n++;
         item[n] = cgen->bframe_opmenu_items.Shadow_Out_item;
-        item_val[n] = AB_LINE_SHADOW_OUT; n++;
+        item_ptr[n] = AB_LINE_SHADOW_OUT; n++;
         item[n] = cgen->bframe_opmenu_items.Shadow_In_item;
-        item_val[n] = AB_LINE_SHADOW_IN; n++;
+        item_ptr[n] = AB_LINE_SHADOW_IN; n++;
         item[n] = cgen->bframe_opmenu_items.Etched_Out_item;
-        item_val[n] = AB_LINE_ETCHED_OUT; n++;
+        item_ptr[n] = AB_LINE_ETCHED_OUT; n++;
         item[n] = cgen->bframe_opmenu_items.Etched_In_item;
-        item_val[n] = AB_LINE_ETCHED_IN; n++;
+        item_ptr[n] = AB_LINE_ETCHED_IN; n++;
         prop_options_init(&(pts->frame), cgen->bframe_opmenu_label,
                             cgen->bframe_opmenu, cgen->bframe_opmenu_menu,
-                            n, item, (XtPointer*)item_val,
+                            n, item, item_ptr,
                             cgen->bframe_cb);
 
         /* Operation */
         n = 0;
         item[n] = cgen->op_rbox_items.Editable_item;
-        item_val[n] = False; n++;
+        item_ptr[n] = False; n++;
         item[n] = cgen->op_rbox_items.Read_Only_item;
-        item_val[n] = True; n++;
+        item_ptr[n] = True; n++;
         prop_radiobox_init(&(pts->op), cgen->op_rbox_label,
-                cgen->op_rbox, n, item, (XtPointer*)item_val,
+                cgen->op_rbox, n, item, item_ptr,
                 cgen->op_cb);
 
         /* Wrap */
@@ -341,13 +342,13 @@ textp_prop_init(
 
 	n = 0;
 	item[n] = cgen->size_opmenu_items.Characters_item;
-	item_val[n] = SIZE_IN_CHARS_KEY; n++;
+	item_ptr[n] = SIZE_IN_CHARS_KEY; n++;
 	item[n] = cgen->size_opmenu_items.Pixels_item;
-	item_val[n] = SIZE_IN_PIXELS_KEY; n++;
+	item_ptr[n] = SIZE_IN_PIXELS_KEY; n++;
 	prop_options_init(&(pts->size_metric), cgen->size_opmenu_label,
 			   cgen->size_opmenu,
 			   cgen->size_opmenu_menu,
-			   n, item, (XtPointer*)item_val,
+			   n, item, item_ptr,
 			   cgen->size_cb);
 
 	/* Initial State */
