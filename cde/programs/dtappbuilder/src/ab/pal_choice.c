06596f9cf81add7533fa2b09f42eec248e6e4dee
Liang Chang <l-chang@users.sourceforge.net>
2021-02-17 07:42:04 +0800

    dtappbuilder: fix issues of property settings for choice.

--- cde/programs/dtappbuilder/src/ab/pal_choice.c
+++ cde/programs/dtappbuilder/src/ab/pal_choice.c
@@ -265,9 +265,10 @@ choice_prop_init(
     DtbRevolvPropDialogInfo	rpd = &(dtb_revolv_prop_dialog);
     PropChoiceSettingsRec    	*pcs = &(prop_choice_settings_rec[type]);
     Widget            		item[6];
-    int            		item_val[6];
+    XtArgVal          		item_val[6];
+    XtPointer 			item_ptr[6];
     Widget			item2[6];
-    int				item2_val[6];
+    XtArgVal				item2_val[6];
     int            		n, j;
     int				i;
 
@@ -342,12 +343,12 @@ choice_prop_init(
         /* Label, Type, Position */
         n = 0;
         item[n] = cgen->labeltype_rbox_items.String_item;
-        item_val[n] = AB_LABEL_STRING; n++;
+        item_ptr[n] = AB_LABEL_STRING; n++;
         item[n] = cgen->labeltype_rbox_items.Graphic_item;
-        item_val[n] = AB_LABEL_GLYPH; n++;
+        item_ptr[n] = AB_LABEL_GLYPH; n++;
         prop_options_init(&(pcs->label_type), cgen->labeltype_rbox_label,
                 cgen->labeltype_rbox, cgen->labeltype_rbox_menu,
-		n, item, (XtPointer*)item_val, cgen->labeltype_cb);
+		n, item, item_ptr, cgen->labeltype_cb);
 
         prop_field_init(&(pcs->label), cgen->label_field_label, 
 		cgen->label_field, cgen->label_cb);
@@ -356,25 +357,25 @@ choice_prop_init(
 
         n = 0;
         item[n] = cgen->labelpos_rbox_items.Above_item;
-        item_val[n] = AB_CP_NORTH; n++;
+        item_ptr[n] = AB_CP_NORTH; n++;
         item[n] = cgen->labelpos_rbox_items.Left_item;
-        item_val[n] = AB_CP_WEST; n++;
+        item_ptr[n] = AB_CP_WEST; n++;
         prop_options_init(&(pcs->label_pos), cgen->labelpos_rbox_label,
                 cgen->labelpos_rbox, cgen->labelpos_rbox_menu,
-                n, item, (XtPointer*)item_val, cgen->labeltype_cb);
+                n, item, item_ptr, cgen->labeltype_cb);
 
 
         /* Choice Type */
         n = 0;
         item[n] = cgen->choicetype_rbox_items.Radio_Box_item;
-        item_val[n] = AB_CHOICE_EXCLUSIVE; n++;
+        item_ptr[n] = AB_CHOICE_EXCLUSIVE; n++;
         item[n] = cgen->choicetype_rbox_items.Check_Box_item;
-        item_val[n] = AB_CHOICE_NONEXCLUSIVE; n++;
+        item_ptr[n] = AB_CHOICE_NONEXCLUSIVE; n++;
         item[n] = cgen->choicetype_rbox_items.Option_Menu_item;
-        item_val[n] = AB_CHOICE_OPTION_MENU; n++;
+        item_ptr[n] = AB_CHOICE_OPTION_MENU; n++;
         prop_options_init(&(pcs->choice_type), cgen->choicetype_rbox_label,
                         cgen->choicetype_rbox, cgen->choicetype_rbox_menu,
-                        n, item, (XtPointer*)item_val,
+                        n, item, item_ptr,
                         cgen->choicetype_cb);
 
 	for (i=0; i < n; i++)
@@ -383,12 +384,12 @@ choice_prop_init(
 	/* Rows/Columns */
         n = 0;
         item[n] = cgen->layout_rbox_items.Columns_item;
-        item_val[n] = AB_ORIENT_VERTICAL; n++;
+        item_ptr[n] = AB_ORIENT_VERTICAL; n++;
         item[n] = cgen->layout_rbox_items.Rows_item;
-        item_val[n] = AB_ORIENT_HORIZONTAL; n++;
+        item_ptr[n] = AB_ORIENT_HORIZONTAL; n++;
         prop_radiobox_init(&(pcs->row_col), cgen->layout_rbox_label,
                 cgen->layout_rbox,
-                n, item, (XtPointer*)item_val,
+                n, item, item_ptr,
                 cgen->layout_cb);
 
         prop_field_init(&(pcs->row_col_count), NULL,
@@ -427,12 +428,12 @@ choice_prop_init(
 	/* Item Label Type */
         n = 0;
         item[n] = cgen->itemlabel_opmenu_items.String_item;
-        item_val[n] = AB_LABEL_STRING; n++;
+        item_ptr[n] = AB_LABEL_STRING; n++;
         item[n] = cgen->itemlabel_opmenu_items.Graphic_item;
-        item_val[n] = AB_LABEL_GLYPH; n++;
+        item_ptr[n] = AB_LABEL_GLYPH; n++;
         prop_options_init(&(pcs->item_label_type), cgen->itemlabel_type_label,
                 cgen->itemlabel_opmenu, cgen->itemlabel_opmenu_menu,
-		n, item, (XtPointer*)item_val, cgen->itemlist_cb);
+		n, item, item_ptr, cgen->itemlist_cb);
 
         for(i=0; i < n; i++)
             XtAddCallback(item[i], XmNactivateCallback,
