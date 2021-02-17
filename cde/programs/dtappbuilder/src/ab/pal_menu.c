c8116cae70e9609b1a03079b1988ed7667e21fbc
Liang Chang <l-chang@users.sourceforge.net>
2021-02-17 19:14:51 +0800

    dtappbuilder: fix issues of property settings for menu.

--- cde/programs/dtappbuilder/src/ab/pal_menu.c
+++ cde/programs/dtappbuilder/src/ab/pal_menu.c
@@ -244,9 +244,10 @@ menu_prop_init(
     DtbRevolvPropDialogInfo	rpd = &(dtb_revolv_prop_dialog);
     PropMenuSettingsRec    	*pms = &(prop_menu_settings_rec[type]);
     Widget            		item[12];
-    int            		item_val[12];
+    XtArgVal       		item_val[12];
+    XtPointer      		item_ptr[12];
     Widget			item2[12];
-    int				item2_val[12];
+    XtArgVal		item2_val[12];
     int            		i, n, j;
 
     if (type == AB_PROP_REVOLVING)
@@ -338,11 +339,11 @@ menu_prop_init(
         /* Tear-off */
         n = 0;
         item[n] = cgen->tearoff_rbox_items.Enabled_item;
-        item_val[n] = True; n++;
+        item_ptr[n] = True; n++;
         item[n] = cgen->tearoff_rbox_items.Disabled_item;
-        item_val[n] = False; n++;
+        item_ptr[n] = False; n++;
         prop_radiobox_init(&(pms->tearoff), cgen->tearoff_rbox_label,
-                cgen->tearoff_rbox, n, item, (XtPointer*)item_val,
+                cgen->tearoff_rbox, n, item, item_ptr,
                 cgen->tearoff_cb);
 
         /* Color */
@@ -367,14 +368,14 @@ menu_prop_init(
                 cgen->itemlabel_field, cgen->itemlist_cb);
         n = 0;
         item[n] = cgen->itemlabeltype_opmenu_items.String_item;
-        item_val[n] = AB_LABEL_STRING; n++;
+        item_ptr[n] = AB_LABEL_STRING; n++;
         item[n] = cgen->itemlabeltype_opmenu_items.Graphic_item;
-        item_val[n] = AB_LABEL_GLYPH; n++;
+        item_ptr[n] = AB_LABEL_GLYPH; n++;
 	item[n] = cgen->itemlabeltype_opmenu_items.Separator_item;
-        item_val[n] = AB_LABEL_SEPARATOR; n++;
+        item_ptr[n] = AB_LABEL_SEPARATOR; n++;
         prop_options_init(&(pms->item_label_type), cgen->itemlabeltype_opmenu_label,
                 cgen->itemlabeltype_opmenu, cgen->itemlabeltype_opmenu_menu,
-                n, item, (XtPointer*)item_val, cgen->itemlist_cb);
+                n, item, item_ptr, cgen->itemlist_cb);
 
         for(i=0; i < n; i++)
             XtAddCallback(item[i], XmNactivateCallback,
@@ -391,26 +392,26 @@ menu_prop_init(
 	/* Item Line Style */
         n = 0;
         item[n] = cgen->linestyle_opmenu_items.None_item;
-        item_val[n] = AB_LINE_NONE; n++;
+        item_ptr[n] = AB_LINE_NONE; n++;
         item[n] = cgen->linestyle_opmenu_items.Etched_In_item;
-        item_val[n] = AB_LINE_ETCHED_IN; n++;
+        item_ptr[n] = AB_LINE_ETCHED_IN; n++;
         item[n] = cgen->linestyle_opmenu_items.Etched_Out_item;
-        item_val[n] = AB_LINE_ETCHED_OUT; n++;
+        item_ptr[n] = AB_LINE_ETCHED_OUT; n++;
         item[n] = cgen->linestyle_opmenu_items.Etched_In_Dash_item;
-        item_val[n] = AB_LINE_ETCHED_IN_DASH; n++;
+        item_ptr[n] = AB_LINE_ETCHED_IN_DASH; n++;
         item[n] = cgen->linestyle_opmenu_items.Etched_Out_Dash_item;
-        item_val[n] = AB_LINE_ETCHED_OUT_DASH; n++;
+        item_ptr[n] = AB_LINE_ETCHED_OUT_DASH; n++;
         item[n] = cgen->linestyle_opmenu_items.Single_Line_item;
-        item_val[n] = AB_LINE_SINGLE_LINE; n++;
+        item_ptr[n] = AB_LINE_SINGLE_LINE; n++;
         item[n] = cgen->linestyle_opmenu_items.Double_Line_item;
-        item_val[n] = AB_LINE_DOUBLE_LINE; n++;
+        item_ptr[n] = AB_LINE_DOUBLE_LINE; n++;
         item[n] = cgen->linestyle_opmenu_items.Single_Dashed_Line_item;
-        item_val[n] = AB_LINE_SINGLE_DASHED_LINE; n++;
+        item_ptr[n] = AB_LINE_SINGLE_DASHED_LINE; n++;
         item[n] = cgen->linestyle_opmenu_items.Double_Dashed_Line_item;
-        item_val[n] = AB_LINE_DOUBLE_DASHED_LINE; n++;
+        item_ptr[n] = AB_LINE_DOUBLE_DASHED_LINE; n++;
         prop_options_init(&(pms->item_line_style), cgen->linestyle_opmenu_label,
                 cgen->linestyle_opmenu, cgen->linestyle_opmenu_menu,
-                n, item, (XtPointer*)item_val,
+                n, item, item_ptr,
                 cgen->itemlist_cb);
  	prop_options_set_value(&(pms->item_line_style), (XtPointer)AB_LINE_ETCHED_OUT, False);
 
