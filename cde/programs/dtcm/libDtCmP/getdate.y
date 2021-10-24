DoktorCranium/cde

commit e0508b31306b01b2ae49767b6db82ef00da5837c

dtcm: change to detect TZ offset way with configure

cde/programs/dtcm/libDtCmP/getdate.y
@@ -40,6 +40,9 @@ static  char sccsid[] = "@(#)getdate.y 1.10 94/11/07 Copyr 1993 Sun Microsystems
 	/*	University of North Carolina at Chapel Hill	*/
 	/*	@(#)getdate.y	2.6	4/20/84	*/
 
+#ifdef HAVE_CONFIG_H
+#include <autotools_config.h>
+#endif
 #include <EUSCompat.h>
 #include <ctype.h>
 #include <string.h>
@@ -67,7 +70,7 @@ static  char sccsid[] = "@(#)getdate.y 1.10 94/11/07 Copyr 1993 Sun Microsystems
 #define STANDARD 2
 #define MAYBE    3
 
-#ifdef SVR4
+#if defined(SVR4) && !HAVE_DECL_TIMEZONE
 extern long timezone;
 #endif
 
@@ -526,11 +529,11 @@ time_t cm_getdate(char *p, struct timeb *now)
 	lptr = p;
 	if (now == ((struct timeb *) NULL)) {
 		now = &ftz;
-#if defined(SVR4) || defined(__OpenBSD__)
+#if defined(SVR4) || HAVE_DECL_TIMEZONE || defined(HAVE_TM_TM_GMTOFF)
 		tod = time(0);
 		lt = localtime(&tod);
 		now->time = lt->tm_sec;
-#ifdef __OpenBSD__
+#ifdef HAVE_TM_TM_GMTOFF
 		now->timezone = - lt->tm_gmtoff / 60;
 #else
 		now->timezone = timezone/60;

cde/programs/dtcm/libDtCmP/timeops.c
@@ -51,6 +51,9 @@
 static char sccsid[] = "@(#)timeops.c 1.13 95/05/02 Copyr 1991 Sun Microsystems, Inc.";
 #endif
 
+#ifdef HAVE_CONFIG_H
+#include <autotools_config.h>
+#endif
 #include <EUSCompat.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -1007,7 +1010,7 @@ init_time(void)
 	_Xltimeparams localtime_buf;
 	_Xgtimeparams gmtime_buf;
 
-#ifdef SVR4
+#if (defined(SVR4) || HAVE_DECL_TIMEZONE) && !defined(HAVE_TM_TM_GMTOFF)
 	/* Fix for QAR 31607 */
 	tzset();
 	if (getenv("TZ") == NULL){

cde/configure.ac
@@ -403,6 +403,9 @@ AC_CHECK_SIZEOF([long])
 AC_CHECK_SIZEOF([long long])
 AC_CHECK_SIZEOF([time_t])
 
+AC_CHECK_DECLS([timezone])
+AC_CHECK_MEMBERS([struct tm.tm_gmtoff])
+
 dnl jpeg
 AC_CHECK_LIB(jpeg, jpeg_read_header, [JPEGLIB="-ljpeg"],
              [AC_MSG_ERROR([libjpeg not found, please install it])],

cde/programs/dtcm/server/rtable4.c
@@ -32,6 +32,9 @@
  * version 4 of calendar manager rpc protocol functions.
  */
 
+#ifdef HAVE_CONFIG_H
+#include <autotools_config.h>
+#endif
 #include <EUSCompat.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -1416,8 +1419,10 @@ extern long *
 _DtCm_rtable_gmtoff_4_svc(void *args, struct svc_req *svcrq)
 {
 	static long gmtoff;
-#if !defined(CSRG_BASED)
+#if !defined(HAVE_STRUCT_TM_TM_GMTOFF)
+# if  !HAVE_DECL_TIMEZONE
 	extern long timezone;
+# endif
 #else
 	_Xltimeparams	 localtime_buf;
 	time_t ctime;
@@ -1427,7 +1432,7 @@ _DtCm_rtable_gmtoff_4_svc(void *args, struct svc_req *svcrq)
 	if (debug)
 		fprintf(stderr, "_DtCm_rtable_gmtoff_4_svc called\n");
 
-#if defined(CSRG_BASED)
+#if defined(HAVE_STRUCT_TM_TM_GMTOFF)
 	ctime = time(NULL);
 	t = _XLocaltime(&ctime, localtime_buf);
 	gmtoff = - t->tm_gmtoff;

commit 9369d1d5b92bce618dd9d2c4fcf5542172c9512a

dtcm: timeops.c/getdate.y: fix fbsd build errors
Commit e0508b31 introduced build errors on FreeBSD.  This corrects
them.

HAVE_DECL_TIMEZONE should only be used to determine whether or not the
'timezone' variable is defined in a header file or whether it must be
specifically 'extern'ed.

On fbsd, a definition exists, but it is a function in libc and not an
integer timezone value that can be mutliplied or divided.

cde/programs/dtcm/libDtCmP/getdate.y
@@ -529,7 +529,7 @@ time_t cm_getdate(char *p, struct timeb *now)
 	lptr = p;
 	if (now == ((struct timeb *) NULL)) {
 		now = &ftz;
-#if defined(SVR4) || HAVE_DECL_TIMEZONE || defined(HAVE_TM_TM_GMTOFF)
+#if defined(SVR4) || (HAVE_DECL_TIMEZONE && !defined(__FreeBSD__)) || defined(HAVE_TM_TM_GMTOFF)
 		tod = time(0);
 		lt = localtime(&tod);
 		now->time = lt->tm_sec;

cde/programs/dtcm/libDtCmP/timeops.c
@@ -1010,7 +1010,7 @@ init_time(void)
 	_Xltimeparams localtime_buf;
 	_Xgtimeparams gmtime_buf;
 
-#if (defined(SVR4) || HAVE_DECL_TIMEZONE) && !defined(HAVE_TM_TM_GMTOFF)
+#if defined(SVR4)
 	/* Fix for QAR 31607 */
 	tzset();
 	if (getenv("TZ") == NULL){